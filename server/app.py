1d4ceef82a0754de997baf97bd3b92bbde6629f2
michael mirhom <micharby73@gmail.com>
2023-09-16 19:51:16 -0500

    completed

--- server/app.py
+++ server/app.py
@@ -17,11 +17,54 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return jsonify([plant.to_dict() for plant in plants])
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(name=data['name'], image=data['image'], price=data['price'])
+        db.session.add(new_plant)
+        db.session.commit()
+        return jsonify(new_plant.to_dict())
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, plant_id):
+        plant = Plant.query.get(plant_id)
+        if not plant:
+            return make_response(jsonify({'error': 'Plant not found'}), 404)
+        return jsonify(plant.to_dict())
+
+
+
+def to_dict(self):
+    return {
+        'id': self.id,
+        'name': self.name,
+        'image': self.image,
+        'price': self.price
+    }
+
+
+
+Plant.to_dict = to_dict
+
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
+
+
+def ensure_test_plant_exists():
+    with app.app_context():
+        plant = Plant.query.get(1)
+        if not plant:
+            new_plant = Plant(name="Test Plant", image="test_image_url", price=19.99)
+            db.session.add(new_plant)
+            db.session.commit()
+
+# Call the function to ensure the plant exists
+ensure_test_plant_exists()
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    image = db.Column(db.String(255), nullable=True)
+    price = db.Column(db.Float, nullable=True)
